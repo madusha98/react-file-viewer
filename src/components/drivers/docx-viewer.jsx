80ff11e8bd160e63661d861f2d82fe7d3e069d72
Madusha Lakruwan <madushalakruwan98@gmail.com>
2021-05-30 17:08:54 +0530

    docx driver replaced id with ref to fix issues when using multiple elements

--- src/components/drivers/docx-viewer.jsx
+++ src/components/drivers/docx-viewer.jsx
@@ -1,41 +1,45 @@
 // Copyright (c) 2017 PlanGrid, Inc.
 
-import React, { Component } from 'react';
+import React, { useEffect, useRef } from 'react';
 import mammoth from 'mammoth';
 
 import 'styles/docx.scss';
 import Loading from '../loading';
 
-export default class extends Component {
-  componentDidMount() {
+const DocxViewer = (props) => {
+  const docxRef = useRef(null);
+  useEffect(() => {
     const jsonFile = new XMLHttpRequest();
-    jsonFile.open('GET', this.props.filePath, true);
+    jsonFile.open('GET', props.filePath, true);
     jsonFile.send();
     jsonFile.responseType = 'arraybuffer';
     jsonFile.onreadystatechange = () => {
       if (jsonFile.readyState === 4 && jsonFile.status === 200) {
-        mammoth.convertToHtml(
-          { arrayBuffer: jsonFile.response },
-          { includeDefaultStyleMap: true },
-        )
-        .then((result) => {
-          const docEl = document.createElement('div');
-          docEl.className = 'document-container';
-          docEl.innerHTML = result.value;
-          document.getElementById('docx').innerHTML = docEl.outerHTML;
-        })
-        .catch((a) => {
-          console.log('alexei: something went wrong', a);
-        })
-        .done();
+        mammoth
+          .convertToHtml(
+            { arrayBuffer: jsonFile.response },
+            { includeDefaultStyleMap: true },
+          )
+          .then((result) => {
+            const docEl = document.createElement('div');
+            docEl.className = 'document-container';
+            docEl.innerHTML = result.value;
+            // document.getElementById('docx').innerHTML = docEl.outerHTML;
+            docxRef.current.innerHTML = docEl.outerHTML;
+          })
+          .catch((a) => {
+            console.log('alexei: something went wrong', a);
+          })
+          .done();
       }
     };
-  }
+  });
 
-  render() {
-    return (
-      <div id="docx">
-        <Loading />
-      </div>);
-  }
-}
+  return (
+    <div ref={docxRef}>
+      <Loading />
+    </div>
+  );
+};
+
+export default DocxViewer;
